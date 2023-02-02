e41f2a6a18954ca49f36ed7af4dd42d4b67e05c1
歐陽毅 <wishnoblog@gmail.com>
2023-02-02 15:11:30 +0800

    更新資料庫

---
+++ Application/Migrations/202302020656044_add_Group.cs
@@ -0,0 +1,55 @@
+﻿namespace Application.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class add_Group : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ApplicationGroups",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ApplicationGroupRoles",
+                c => new
+                    {
+                        ApplicationRoleId = c.String(nullable: false, maxLength: 128),
+                        ApplicationGroupId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.ApplicationRoleId, t.ApplicationGroupId })
+                .ForeignKey("dbo.ApplicationGroups", t => t.ApplicationGroupId, cascadeDelete: true)
+                .Index(t => t.ApplicationGroupId);
+            
+            CreateTable(
+                "dbo.ApplicationUserGroups",
+                c => new
+                    {
+                        ApplicationUserId = c.String(nullable: false, maxLength: 128),
+                        ApplicationGroupId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.ApplicationUserId, t.ApplicationGroupId })
+                .ForeignKey("dbo.ApplicationGroups", t => t.ApplicationGroupId, cascadeDelete: true)
+                .Index(t => t.ApplicationGroupId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ApplicationUserGroups", "ApplicationGroupId", "dbo.ApplicationGroups");
+            DropForeignKey("dbo.ApplicationGroupRoles", "ApplicationGroupId", "dbo.ApplicationGroups");
+            DropIndex("dbo.ApplicationUserGroups", new[] { "ApplicationGroupId" });
+            DropIndex("dbo.ApplicationGroupRoles", new[] { "ApplicationGroupId" });
+            DropTable("dbo.ApplicationUserGroups");
+            DropTable("dbo.ApplicationGroupRoles");
+            DropTable("dbo.ApplicationGroups");
+        }
+    }
+}
